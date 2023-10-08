9860039e071f0e99e21806f519d5f0ed1dc364c4
Peerapat Sattapornnara <93782805+Phepattt2@users.noreply.github.com>
2023-10-08 08:58:50 +0700

    Add files via upload

---
+++ Ex6/Controllers/QuizController.cs
@@ -0,0 +1,23 @@
+﻿using Ex_6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ex_6.Controllers
+{
+    public class QuizController : Controller
+    {
+        // GET: QuizController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submitted(FormData formData)
+        {
+            return View(formData);
+        }
+
+    }
+}
